ddb69d4ad7109da537d66dfa669b2a4a960d2865
Amaury <amaurycb@gmail.com>
2024-06-04 20:06:10 +0200

    add Array to store the files that match the pattern

--- check_patterns.sh
+++ check_patterns.sh
@@ -6,6 +6,9 @@ pattern=$(echo "${INPUT_PATTERN}")
 #Define if deep check is needed
 deep=$(echo "${INPUT_DEEP}")
 
+# Array to store the files that match the pattern
+matching_files=()
+
   # Get a list of all files in the repository
 if [ $deep -eq 0 ]; then
   # Obtener el hash del último commit
@@ -16,8 +19,7 @@ else
   changed_files=$(git ls-files)
 fi
 
-# Flag to indicate if any pattern is found
-pattern_found=0
+
 
 # Loop through each changed file
 for file in $changed_files; do
@@ -28,11 +30,13 @@ for file in $changed_files; do
     if grep -Eq "$pattern" "$file"; then
       echo "Pattern '$pattern' found in file '$file'"
       pattern_found=1
+      # Add the file to the array of matching files
+      matching_files+=("$file")
     fi  
 done
 
 # Exit with a non-zero status if any pattern is found
-if [ $pattern_found -ne 0 ]; then
+if [ ${#matching_files[@]} -ne 0 ]; then
   echo "Pattern(s) found, failing the check."
   exit 1
 else
